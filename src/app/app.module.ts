5a372122e1ab9326db37b8894a59e0cb69de896f
Giorgio Mazzei <giorgiomazzei@Giorgios-MacBook-Pro.local>
2018-02-11 18:18:51 +0000

    material angular in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-
+// Components
 import { AppComponent } from './app.component';
+import { AppBarComponent } from './components/appBar.component';
+import {
+  MatTabsModule,
+  MatFormFieldModule,
+  MatSortModule,
+  MatRadioModule,
+  MatCardModule,
+  MatIconModule,
+  MatDialogModule,
+  MatTableModule,
+  MatSliderModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatChipsModule,
+  MatListModule,
+  MatInputModule,
+  MatTooltipModule,
+  MatMenuModule,
+  MatSelectModule
+} from '@angular/material';
+// External modules
+import 'hammerjs';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatSortModule,
+    MatRadioModule,
+    MatCardModule,
+    MatIconModule,
+    MatDialogModule,
+    MatTableModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatListModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatMenuModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/appBar.component.ts
@@ -0,0 +1,11 @@
+// ANGULAR COMPONENTS
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'appBar',
+    templateUrl: '../templates/appBar.component.html',
+    styleUrls: ['../styles/appBar.component.css']
+})
+export class AppBarComponent {
+    constructor() { }
+}
